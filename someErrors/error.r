2bb247fe505db76ec39d5effda97dd7d3ff9e33c
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-11-01 15:03:58 +0500

    Errors

---
+++ someErrors/error.r
@@ -0,0 +1,3 @@
+Some times issues occured with "doesn't usb card"  or "forget password"
+and some times must be add accept other server programm "PrinterController.exe", "DgaMonitor.exe" or "DbParser.exe"
+TechAssist must be check their working
---
+++ someErrors/telegrammIssues.r
@@ -0,0 +1,4 @@
+Some user's ask telegram doesn't block or doesn't check it 
+at this situation check the telegram desktop because 
+telegram desktop running by Datagaze not others 
+Datagaze has own telegram and it must be replaced to offical on PC 
