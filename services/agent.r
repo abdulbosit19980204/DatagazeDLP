19a987b017f5f719692ac17a5472c3385fc94fa0
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-11-01 14:09:58 +0500

    Services

---
+++ services/agent.r
@@ -0,0 +1,2 @@
+To check status of the agent on windows. 
+"DbParser"=> command must be run on the "cmd" or "win+R" 
---
+++ services/protsess.r
@@ -0,0 +1,8 @@
+In this part we will check server's status
+
+1. to check status
+    "pm2 status all"
+2. check the mongod (Mongodb) is running. the reason is used Mongodb on Datagaze DLP
+    "systemctl status mongod"
+3. check the logs 
+    "pm2 logs socket"
