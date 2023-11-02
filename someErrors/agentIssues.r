097f279edeee27915dfdaca915959234c7aa1c43
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-11-02 11:44:52 +0500

    Agent's problems

---
+++ someErrors/agentIssues.r
@@ -0,0 +1,6 @@
+This problem up to IP address. 
+At this time agent's installer given incorrect IP idress. it is not same to Servers ip addres. 
+to check this we can use DbParser by DatagazeDLP. 
+DbParser has Configurations field and in this field's Main.ServerAddress  save server's IP address.
+
+The second problemm with connection agent and server 
