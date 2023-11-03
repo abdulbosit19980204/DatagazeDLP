cf1c2a79e245d979ff79b67bad1512bdf61a193f
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-11-03 14:55:37 +0500

    Computer Freeze issues

---
+++ someErrors/computerFreeze:.r
@@ -0,0 +1,4 @@
+Some clients say "my computers are freezes by agent" but this programm doesn't freez. 
+at this time run the "sturt up" and sort by using RAM, CPU. and stop them. 
+DLP uses three service. They are "DgAgent","DgHostService" and "DgaMonitor".
+If one of them stop, DLP doesn't work properly (correct)
