da015f2272fc52ee103cb28e4f20eb9eab2df267
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-11-07 16:12:55 +0500

    Dashboard issues

---
+++ someErrors/dashbordIssues.r
@@ -0,0 +1,18 @@
+Bazan web panel bilan ham muammolar sodir bolishi mumkin
+
+1. Dashboard (Web-panel) ochilmayapti
+2. Dashboardga kirmayapti
+3. Dashboard oslib qolyapti kabi shikoyatlar ham yuzaga kelishi mumkin 
+
+Bunday muammolarni Agentga aloqasi yoq 
+shuning uchun Serverni tekshirishini boshlashimiz mumkin
+
+1. Kop xollarda MongoDB ochib qolgan bolishi mumkin
+2. MongoDB ochib qolgan bolsa qaytadan ishga tushirishimiz kerak boladi
+
+Mongoni tekshirish
+
+1. systemctl status mongod 
+2. systemctl start mongod
+
+komandalaridan foydalanamiz
