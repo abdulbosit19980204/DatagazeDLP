0be238e4a765004f2b831035ef4fd8d634026059
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-11-07 16:16:34 +0500

    Usb isssues

---
+++ someErrors/usbIssues.r
@@ -0,0 +1,9 @@
+Avval bloklangan USBni blokdan chiqarib bo'lmayapti
+
+Bazi xollarda mijozlar orasida avvalgi bloklangan usb portlarni blockdan 
+chiqarishda tushunmovchiliklar xam yuzaga kelishi mumkin
+
+Bunday muammo asosan server bilan agent orasidagi connectionlarda boladi 
+
+1. Agent online yoki offline ligini tekshrish kerak boladi 
+2. Muammo asosan socket moduli bilan bolishi ehtimoli yuqori
