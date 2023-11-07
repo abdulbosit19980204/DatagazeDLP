72a0d9406b33641d6fbf4dacf38eadf0e92b86bc
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-11-07 16:06:13 +0500

    online Offline status issues

---
+++ someErrors/onlineOfflineIssues.r
@@ -0,0 +1,7 @@
+Xodimlar ish joyida lekin web-panelda "offline" bo'lib turibti
+
+bu kabi muammolar ham bazan uchrab turadi buning uchun 
+dastlab socket log larini tekishrib korishimiz kerak boladi 
+
+"pm2 logs socket"
+
