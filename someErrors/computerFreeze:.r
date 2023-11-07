8c751b1674d2d297ac039ee1471971822685a061
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-11-07 15:33:13 +0500

    Screening Issues

--- someErrors/computerFreeze:.r
+++ someErrors/computerFreeze:.r
@@ -2,3 +2,4 @@ Some clients say "my computers are freezes by agent" but this programm doesn't f
 at this time run the "sturt up" and sort by using RAM, CPU. and stop them. 
 DLP uses three service. They are "DgAgent","DgHostService" and "DgaMonitor".
 If one of them stop, DLP doesn't work properly (correct)
+
---
+++ someErrors/screenIssues.r
@@ -0,0 +1,13 @@
+Xodimdan skrinshotlar / ma'lumotlar kelmayapti
+
+1. Muammo dastlab agendami yoki serverdami shuni tekshirib olamiz
+
+2. Dastlab ishni agentni tekshirishdan boshlagan yaxshi
+3. Skrinshot olinganmi yoqmi shuni tekshiramiz 
+    Buning uchun DbParser ni ochamiz va skrin bolimini korib chiqamiz
+4. Agar skrinshot olingan bolsa keyingi qadam jonatilganmi yoqmi shuni tekshirish 
+    Buning uchun Log bolimini korib chiqamiz
+5. Skrinshotlar serverga jonatilgan bolsa lekin serverda qabul qilinmagan bolsa 
+    sereverni tekshirishni boshlaymiz
+6. Buning uchun Serverdagi service larni tekshirish kerak 
+7. Bazan DbParser dagi Screen bolimini ham Error bolimini ham korib chiqish kerak boladi
